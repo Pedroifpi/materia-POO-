d28c6810ea2e908f218bd06549ea196f2ec983fe
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2025-04-23 08:33:13 -0300

    Create contabancaria.java
    ty

---
+++ contabancaria.java
@@ -0,0 +1,17 @@
+public class contabancaria {
+    private String numero;
+    private String titular;
+    private double saldo;
+    public double sacar(){
+
+        return 0;
+
+    }
+    public double depositar(){
+
+        return 0;
+
+    }
+
+
+}
