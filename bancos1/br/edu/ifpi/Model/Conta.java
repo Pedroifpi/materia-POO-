c94400d2b43f9ce714b42397267629a338454a57
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2025-07-18 08:48:52 -0300

    new
    nex

--- bancos1/br/edu/ifpi/Model/Conta.java
+++ bancos1/br/edu/ifpi/Model/Conta.java
@@ -14,5 +14,16 @@ public class Conta {
     public double getSaldo() {
         return saldo;
     }
-
+    public void depositar(double valor){
+        if (valor > 0) {
+            saldo += valor;
+        }
+    }
+    public boolean sacar(double valor){
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
 }
